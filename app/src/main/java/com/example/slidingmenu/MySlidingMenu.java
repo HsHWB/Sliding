affc0035b4627e16d2541fe23f5f9262675a72fa
HsRise <hswbinforgithub@gmail.com>
2015-03-09 21:10:03 +0800

    #自定义HorizontalScrollView实现sliding效果

---
+++ app/src/main/java/com/example/slidingmenu/MySlidingMenu.java
@@ -0,0 +1,18 @@
+package com.example.slidingmenu;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+/**
+ * Created by Hs on 2015/3/9.
+ */
+public class MySlidingMenu extends HorizontalScrollView {
+
+    public MySlidingMenu(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    
+
+}
